3530dc6bb68095786ed25466399336799626846b
lvndry <monga.4landry@gmail.com>
2018-03-31 14:13:13 +0200

    change layout

--- gui/gui.py
+++ gui/gui.py
@@ -18,16 +18,17 @@ def change_master(slider_value):
     volume1_text.value = volume2_text.value = slider_value
 
 app = App(title="Zikubes", bg="brown", height=480, width=800)
-box_slider = Box(app, layout="grid", grid=[2,2])
 
-welcome_message = Text(app, text="Welcome to Zikubes", size=40, color="lightblue", bg="white")
-cubes_num = Text(app, text="You have " + str(cubes) + " cubes open")
+welcome_message = Text(app, text="Welcome to Zikubes", size=40, color="lightblue", bg="brown")
 
-volume1_text = Text(app, text=volume1, size=30, color="green", bg="white")
-volume2_text = Text(app, text=volume2, size=30, color="red", bg="white")
+volume1_text = Text(app, text=volume1, size=30, color="green", bg="brown")
+volume2_text = Text(app, text=volume2, size=30, color="red", bg="brown")
+
+box_slider = Box(app, layout="grid")
 
 volume1_slider = Slider(box_slider, horizontal=False, command=change_volume1, start=100, end=0, grid=[0,0])
 volume2_slider = Slider(box_slider, horizontal=False, command=change_volume2, start=100, end=0, grid=[2,0])
 master_slider = Slider(box_slider, horizontal=False, command=change_master, start=100, end=0, grid=[4,0])
+volume1_slider.text_size = volume2_slider.text_size = master_slider.text_size = 1
 
 app.display()
