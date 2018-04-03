f264d4e2ea1fd9ad86f082dbcd0e899df36bd5d7
Niitch <37935667+Niitch@users.noreply.github.com>
2018-04-03 15:58:27 +0200

    Add files via upload

---
+++ sound/audio.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+import random
+
+lead=['Do','Mib','Fa','Sol','Sib','Do2']
+drum=['Kick','Snare']
+
+playsound(random.choice(lead)+'.wav')
+playsound(random.choice(lead)+'.wav')
+playsound(random.choice(lead)+'.wav')
+playsound(random.choice(drum)+'.wav')
+playsound(random.choice(drum)+'.wav')
